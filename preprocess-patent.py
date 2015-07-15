6290d63260b30d8104e97c314d1380e0e0798250
mbartoli <bartolimichael@gmail.com>
2015-07-15 15:14:55 -0400

    preprocess patent.csv

---
+++ preprocess-patent.py
@@ -0,0 +1,15 @@
+'''
+patent,Cit_Date,Cit_Name,Cit_Kind,Cit_Country,Citation,Category,CitSeq
+06009555,1980-03-01,FAY,"",,04192017,,
+06009555,1985-10-01,"BROWN ET AL.","",,04547903,,
+'''
+
+import csv
+
+with open("patent.csv","rb") as source:
+    rdr= csv.reader(source)
+    with open("patent-processed.csv","wb") as result:
+        wtr= csv.writer(result, delimiter=",")
+        for r in rdr:
+            wtr.writerow((r[0], r[6], r[8]))
+
