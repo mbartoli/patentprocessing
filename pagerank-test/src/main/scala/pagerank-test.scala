d68ca645437714a04829ab8887282ded97ed3b76
Mike Bartoli <mbartoli@users.noreply.github.com>
2015-07-29 15:10:18 -0400

    PageRank working on test data and saves to HDFS

--- pagerank-test/src/main/scala/pagerank-test.scala
+++ pagerank-test/src/main/scala/pagerank-test.scala
@@ -6,22 +6,25 @@ import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 
 object PageRank {
-	def main(args: Array[String]) {
-		val conf = new SparkConf().setAppName("PatentPagerank")
-    		val sc = new SparkContext(conf)
-		// Load the edges as a graph
-		val graph = GraphLoader.edgeListFile(sc, "hdfs:///data/patents/citation-processed.csv")
-		// Run PageRank
-		val ranks = graph.pageRank(0.01).vertices
-		// Join the ranks with the usernames
-		val users = sc.textFile("hdfs:///data/patents/patents-processed.csv").map { line =>
-		  val fields = line.split(",")
-		  (fields(0).toLong, fields(1))
-		}
-		val ranksByUsername = users.join(ranks).map {
-		  case (id, (username, rank)) => (username, rank)
-		}
-		// Print the result
-		println(ranksByUsername.collect().mkString("\n"))
-	}
+        def main(args: Array[String]) {
+                val conf = new SparkConf().setAppName("PatentPagerank")
+                val sc = new SparkContext(conf)
+                // Load the edges as a graph
+                //val graph = GraphLoader.edgeListFile(sc, "hdfs:///data/patents/citation-processed.csv")
+                val graph = GraphLoader.edgeListFile(sc, "hdfs:///data/patents/example/followers.txt")
+                // Run PageRank
+                val ranks = graph.pageRank(0.01).vertices
+                // Join the ranks with the usernames
+                val users = sc.textFile("hdfs:///data/patents/example/users.txt").map { line =>
+                //val users = sc.textFile("hdfs:///data/patents/patents-processed.csv").map { line =>
+                  val fields = line.split(",")
+                  (fields(0).toLong, fields(1))
+                }
+                val ranksByUsername = users.join(ranks).map {
+                  case (id, (username, rank)) => (username, rank)
+                }
+                // Collect and print the result
+                val count = ranksByUsername.collect()
+                sc.parallelize(count).saveAsTextFile("hdfs:///data/patents/pagerank-output")
+        }
 }
