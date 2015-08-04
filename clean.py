678088900922e20d641a05f0fc3c0757a3ee80b0
mbartoil <bartolimichael@gmail.com>
2015-08-04 13:27:16 -0400

    regex print on match

--- clean.py
+++ clean.py
@@ -1,6 +1,7 @@
 import re
 
-with open('/home/vagrant/patentdata/patent-1.txt'):
-	for line in f:
-		match = regex.search(line)
-		print match.group(1)
+f = open('/home/vagrant/patentdata/patent-1.txt', 'r'):
+	
+for line in f:	
+	if re.match("^\d+\ \d+$", line):
+		print line
